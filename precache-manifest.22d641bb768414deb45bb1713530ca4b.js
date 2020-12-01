4a6ceed2eb1e053dd1f3b77a599621079919ce6b
woojin624 <624jang@gmail.com>
2020-12-01 16:26:37 +0900

    Updates

---
+++ precache-manifest.22d641bb768414deb45bb1713530ca4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb420ec77c724de7c504c0a1d783f67",
+    "url": "/wootube/index.html"
+  },
+  {
+    "revision": "82ccbc2b90fb49220a56",
+    "url": "/wootube/static/css/main.08e15257.chunk.css"
+  },
+  {
+    "revision": "0e4e39c5fd4b3f56b0f7",
+    "url": "/wootube/static/js/2.5c0c0119.chunk.js"
+  },
+  {
+    "revision": "20d7947c3627dad54ba9a5db3b4b7abb",
+    "url": "/wootube/static/js/2.5c0c0119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82ccbc2b90fb49220a56",
+    "url": "/wootube/static/js/main.34242096.chunk.js"
+  },
+  {
+    "revision": "4d3a728c2e2bdcc5fd08",
+    "url": "/wootube/static/js/runtime-main.6f3adbd1.js"
+  }
+]);
